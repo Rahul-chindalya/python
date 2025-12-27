9fc41d487f1fee67a334340ed3275976c7fd8c97
Rahul-chindalya <rahulchindalya01@gmail.com>
2025-12-28 01:05:48 +0530

    Built in module

---
+++ 16_modules/built_in_modules/demo.py
@@ -0,0 +1,26 @@
+# random
+# datetime
+# sys -> devops 
+# os -> devops
+# csv -> data analytics
+# json -> full stack 
+
+import datetime
+print(datetime.datetime.now())
+# Random Module
+import random
+
+print(random.random())
+
+print(random.randint(1,10))
+
+# Simulate OTP functionality
+print(random.randrange(100000)) 
+
+# Simulate Lottery App
+list_lottery = ["LOT1","LOT2","LOT3","LOT4","LOT5"]
+print(random.choice(list_lottery))
+
+# Simulate Random Pickup 
+list_users = ["us1","us2","us3","us4","us5","us6","us7","us8","us9","us10"]
+print(random.choices(list_users,k=3))
