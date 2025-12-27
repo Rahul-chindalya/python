b2308fe376b93abfa10de9d1a76e694ed1fe5960
Rahul-chindalya <rahulchindalya01@gmail.com>
2025-12-28 01:09:22 +0530

    external module

---
+++ 16_modules/external_module/demo.py
@@ -0,0 +1,8 @@
+# Hit an External API (Github API)
+import requests
+response = requests.get("https://api.github.com")
+print(response)
+
+# Use pip to install the external.py file 
+# 
+#  pip install requests 
